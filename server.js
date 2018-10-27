da383ec1dcfa763b58b2a0deeb2369b3cd7235d1
NevMem <memlolkek@gmail.com>
2018-10-27 15:10:12 +0300

    CORS"

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ db.connect(dbUrl)
         next()
     })
 
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        next()
+    })
+
     app.use(bParser.json())
     app.use(bParser.urlencoded({extended: true}))
 
