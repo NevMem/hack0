98b5fd45912277cd872d3ceced102665018c5712
DebNatkh <debnatkh@gmail.com>
2018-10-27 13:55:41 +0300

    Leave and join options

--- telegram_front/serve.py
+++ telegram_front/serve.py
@@ -11,14 +11,15 @@ def offset_print(s, offset):
 bot_url = "https://api.telegram.org/bot"
 api_url = "http://172.31.19.207/"
 
-def get_updates_json(request):
-    response = requests.get(request + 'getUpdates')
+def get_updates_json(request, params = dict()):
+    response = requests.get(request + 'getUpdates', params)
     return response.json()
 
 
 def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
+    if (total_updates == -1): return {'update_id':-1}
     return results[total_updates]
 
 def get_chat_id(update):
@@ -55,6 +56,12 @@ def join(data):
     response = requests.post(url, data=data).json()
     return response
 
+def leave(data):
+    url = api_url + "leave"
+    offset_print("Request to " + url + "\n" + str(data), 2)
+    response = requests.post(url, data=data).json()
+    return response
+
 def myposition(data):
     url = api_url + "myposition"
     offset_print("Request to " + url + "\n" + str(data), 2)
@@ -70,14 +77,18 @@ def main():
     print(bot_url)
 
 
-    # print(initiate_room(auth({'login' : 'admin', 'password' : 'admin'})))
+    response = (initiate_room(auth({'login' : 'admin', 'password' : 'admin'})))
+    print(response)
 
     tokensdb = dict() # To be replaced with database requests
     pindb = dict() # To be replaced with database requests
     
     update_id = last_update(get_updates_json(bot_url))['update_id']
     while True:
-        update = last_update(get_updates_json(bot_url))
+        update = (get_updates_json(bot_url, {'offset' : None}))
+        if (len(update) == 0): continue
+        update = last_update(update);
+        # print (update_id)
         if (update['update_id'] != update_id):
             uid = get_chat_id(update)
             update_id = update['update_id']
@@ -104,15 +115,64 @@ def main():
                     try:
                         response = myposition({'token' : tokensdb[uid], 'pin' : pindb[uid]})
                         if ('position' in response.keys()):
-                            message_text = "There are " + str(response['position']) + " people before you"
+                            message_text = "There are " + str(response['position'] - 1) + " people before you"
                         else:
                             message_text = "Failed! an error occured\n" + response['err']
                     except Exception as e:
                         message_text = "Failed! an error occured\n" + str(e)
-                send_mess(uid, message_text, {'reply_markup' : '{"keyboard":[["Position","Leave"]]}'})
-            else:
-                # Request to leave group
-                send_mess(uid, 'You have left the room :)')
+                send_mess(uid, message_text, {'reply_markup' : '{"keyboard":[["Position","Leave"]]  }'})
+            elif (command['command'] == 'Leave'):
+                message_text = ""
+                if (uid in tokensdb.keys()):
+                    try:
+                        response = leave({'pin' : pindb[uid], 'token' : tokensdb[uid]})
+                        if ('err' not in response.keys()):
+                            message_text = "Successfully left room #" + pindb[uid]
+                            pindb.pop(uid)
+                        else:
+                            message_text = "Failed! an error occured\n" + response['err']
+                    except Exception as e:
+                        message_text = "Failed! an error occured\n" + str(e)
+                else:
+                    message_text = "You have not joind any room yet"
+                if (uid in pindb.keys()):
+                    send_mess(uid, message_text, {'reply_markup' : '{"keyboard":[["Position","Leave"]]}'})
+                else:
+                    send_mess(uid, message_text)
+            # elif (command['command'] == '/authorize'):
+            #     args = command['args'].strip().split()
+            #     message_text = ""
+            #     if (len(args) != 2) :
+            #         message_text = "Failed, please provide login and password i.e. /authorize login password"
+            #     else:
+            #         try:
+            #             response = auth({'login' : args[0], 'password' : args[-1]})
+            #             if ('token' in response.keys()):
+            #                 tokensdb[uid] = response['token']
+            #             else:
+            #                 message_text = "Failed! an error occured\n" + response['err']
+            #         except Exception as e:
+            #             message_text = "Failed! an error occured\n" + str(e)
+            #     send_mess(uid, message_text)
+            # elif (command['command'] == '/newroom'):
+            #     message_text = ""
+            #     if (uid not in tokensdb.keys()):
+            #         message_text = "You have not joind any room or authorized yet"
+            #     else:
+            #         try:
+            #             response = initiate_room(tokensdb[uid])
+            #             if ('pin' in response.keys()):
+            #                 pindb[tokensdb[uid]] = response['pin']
+            #                 message_text = "You have successfully created room #" + str(response['pin'])
+            #             else:
+            #                 message_text = "Failed! an error occured\n" + response['err']
+            #         except Exception as e:
+            #             message_text = "Failed! an error occured\n" + str(e)
+            #     if ("successfully" in message_text):
+            #         send_mess(message_text)
+
+
+
 
 
 if __name__ == '__main__':
